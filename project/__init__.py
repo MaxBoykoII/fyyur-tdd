d6035c5e13ee0463c58dc797a63682baa16487ec
Max Boyko <maxboykomax@gmail.com>
2020-03-09 00:44:00 -0700

    feat: Containerize the application

--- project/__init__.py
+++ project/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask, jsonify
 from flask_restplus import Resource, Api
+import os
 
 app = Flask(__name__)
 
 api = Api(app)
 
 # set config
+app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object('project.config.DevelopmentConfig')
 
 class Ping(Resource):
