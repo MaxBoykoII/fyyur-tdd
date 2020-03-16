18974504091fc44481d3b11f958c5635d075f0cc
Max Boyko <maxboykomax@gmail.com>
2020-03-15 23:52:39 -0700

    chore: Add command for seeding the database

--- manage.py
+++ manage.py
@@ -12,5 +12,29 @@ def recreate_db():
     db.create_all()
     db.session.commit()
 
+@cli.command('seed_db')
+def seed_db():
+    db.session.add(Artist(
+        name='Jonny Cash',
+        city='New York',
+        state='NY',
+        phone='674-674-674',
+        genres='Folk',
+        image_link=None,
+        facebook_link=None))
+    
+    db.session.add(Artist(
+        name='Bobby Blue Bland',
+        city='San Francisco',
+        state='CA',
+        phone='216-216-216',
+        genres='Blues',
+        image_link=None,
+        facebook_link=None))
+    
+    db.session.commit()
+
+    
+
 if __name__ == '__main__':
     cli()
