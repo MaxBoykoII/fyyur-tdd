0286153229a58c46f44ad9f5cdf9935d0afcb0ad
Max Boyko <maxboykomax@gmail.com>
2020-03-15 17:04:09 -0700

    feat: Add static assets and implement home route

--- project/__init__.py
+++ project/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 # instantiate the db
@@ -19,8 +19,17 @@ def create_app(script_info=None):
     # set up extensions
     db.init_app(app)
 
+    # set home route
+    '''
+    @app.route('/')
+    def index():
+        return render_template('pages/home.html')
+    '''
+
     # register blueprints
+    from project.api.home import home_blueprint
     from project.api.ping import ping_blueprint
+    app.register_blueprint(home_blueprint)
     app.register_blueprint(ping_blueprint)
 
     # shell context for flask cli
